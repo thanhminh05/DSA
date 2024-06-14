fcb8d59fb6eadc987a192108a54dbf8e751a209d
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2024-06-14 20:29:23 +0700

    lesson 20. Ex 5

---
+++ LESSON_20/4.bubbleSort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+
+  int n;
+  cin >> n;
+  int a[n];
+  for (int &x : a) {
+    cin >> x;
+  }
+
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - 1 - i; j++) {
+      if (a[j] > a[j + 1]) {
+        swap(a[j], a[j + 1]);
+      }
+    }
+
+    cout << "Step " << i + 1 << ": ";
+    for (int x : a) {
+      cout << x << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
+
+// 4
+// 5 7 3 2
+
+// Step 1: 2 7 5 3
+// Step 2: 2 3 7 5
+// Step 3: 2 3 5 7
