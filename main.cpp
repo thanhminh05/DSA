aeba0a3b71789c6b8d7774d9c7576c700a6b7bb0
thanhminhspk05 <69253365+thanhminhspk05@users.noreply.github.com>
2024-06-16 09:53:45 +0700

    euler function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <bits/stdc++.h>
 #include <iostream>
 
+#define ll long long
 using namespace std;
+
+ll gcd(ll a, ll b) {
+  if (b == 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+}
 
 int main() {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  std::cin.tie(NULL);
 
 #ifndef ONLINE_JUDGE
   freopen("input.txt", "r", stdin);
@@ -14,17 +22,20 @@ int main() {
 
   int n;
   cin >> n;
-  int count = 0;
 
-  for (int i = 1; i <= sqrt(n); i++) {
-    if (i * i == n) {
-      count += 1;
-    } else if (n % i == 0) {
-      count += 2;
+  ll res = n;
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) {
+      while (n % i == 0) {
+        n = n / i;
+      }
+      res -= res / i;
+    }
+    if (n != 1) {
+      res -= res / n;
     }
   }
-
-  cout << count;
+  cout << res;
 }
 
 // 20
