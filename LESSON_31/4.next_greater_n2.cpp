ab08af0a1152d55fe6e7c8c8bbb5c386648d4101
Nguyen Thanh Minh <thanhminhspk05@gmail.com>
2024-06-24 11:53:23 +0700

    save

--- LESSON_31/4.next_greater_n2.cpp
+++ LESSON_31/4.next_greater_n2.cpp
@@ -14,11 +14,21 @@ int main() {
 #endif
   int n;
   cin >> n;
-  int a[n], b[n];
+  int a[n];
   for (int i = 0; i < n; i++) {
     cin >> a[i];
   }
-  stack<int> st;
+
+  for (int i = 0; i < n; i++) {
+    int res = -1;
+    for (int j = i + 1; j < n; j++) {
+      if (a[j] > a[i]) {
+        res = a[j];
+        break;
+      }
+    }
+    cout << res << " ";
+  }
 
   return 0;
 }
--- LESSON_31/5.next_greater_stack.cpp
+++ LESSON_31/5.next_greater_stack.cpp
@@ -20,6 +20,28 @@ int main() {
   }
   stack<int> st;
 
+  for (int i = 0; i < n; i++) {
+    if (st.empty()) {
+      st.push(i);
+    } else {
+      while (!st.empty() && a[st.top()] < a[i]) {
+        b[st.top()] = a[i];
+        st.pop();
+      }
+      st.push(i);
+    }
+    cout << st.size() << endl;
+  }
+
+  while (!st.empty()) {
+    b[st.top()] = -1;
+    st.pop();
+  }
+
+  for (int x : b) {
+    cout << x << " ";
+  }
+
   return 0;
 }
 
